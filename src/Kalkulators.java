3d8616998b73edbc513590860cd61ae0dcc2cd0f
Mazrims <tomassmazrims85@gmail.com>
2025-04-02 08:17:33 +0300

    Nodrošināta iespēja izveidot studenta masīvu

--- src/Kalkulators.java
+++ src/Kalkulators.java
@@ -1,9 +1,22 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Kalkulators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int studSk, kritSk;
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat(" 0.#");
+		
+		do {
+			System.out.println("Cik studentiem aprēķināt gala vērtējumu?");
+			while(!scan.hasNextInt()) {
+				System.out.println("Cik studentiem aprēķināt gala vērtējumu?");
+				scan.next();
+			}
+			studSk = scan.nextInt();
+		}while(studSk<1);
+		String[] studenti = new String[studSk];
 
 	}
-
 }
